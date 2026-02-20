c64c37c589d804ff91393192379ad94123148fc2
unknown <19bcs2301@gmail.com>
2026-02-20 22:43:24 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
+import os
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.router import api_router
+from app.core.exceptions import http_exception_handler
 from app.db.base import Base
 from app.db.session import engine
-from app.core.exceptions import http_exception_handler
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="HRMS Lite API")
 
-# ✅ CORS CONFIG — ADD THIS
+# Create tables ONLY in local/dev
+if os.getenv("ENV") != "production":
+    Base.metadata.create_all(bind=engine)
+
 origins = [
     "http://localhost:3000",
     "http://localhost:5173",
@@ -20,13 +22,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # or ["*"] for development
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Existing setup
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.include_router(api_router, prefix="/api/v1")
 
